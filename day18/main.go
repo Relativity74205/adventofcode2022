b872d0dd3be39bb26bb820821ff6a0519a0d49df
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-03-11 23:04:41 +0100

    feat: added day18 part2 (WIP)

--- day18/main.go
+++ day18/main.go
@@ -11,19 +11,20 @@ type Coords struct {
 	x, y, z int
 }
 
+type Space struct {
+	xMin, xMax, yMin, yMax, zMin, zMax int
+}
+
 func evalA(lines []string) int {
-	drops := make(map[Coords]bool)
+	drops := getDropCoords(lines)
+	totalSurface := getTotalSurface(drops)
 
-	for _, line := range lines {
-		parts := strings.Split(line, ",")
-		x, _ := strconv.Atoi(parts[0])
-		y, _ := strconv.Atoi(parts[1])
-		z, _ := strconv.Atoi(parts[2])
-		drops[Coords{x, y, z}] = true
-	}
+	return totalSurface
+}
 
+func getTotalSurface(drops map[Coords]bool) int {
 	totalSurface := 0
-	for drop, _ := range drops {
+	for drop := range drops {
 		surface := 6
 		if drops[Coords{drop.x + 1, drop.y, drop.z}] {
 			surface--
@@ -45,13 +46,130 @@ func evalA(lines []string) int {
 		}
 		totalSurface += surface
 	}
-
 	return totalSurface
 }
 
-func evalB(lines []string) int {
+func getDropCoords(lines []string) map[Coords]bool {
+	drops := make(map[Coords]bool)
 
-	return 0
+	for _, line := range lines {
+		parts := strings.Split(line, ",")
+		x, _ := strconv.Atoi(parts[0])
+		y, _ := strconv.Atoi(parts[1])
+		z, _ := strconv.Atoi(parts[2])
+		drops[Coords{x, y, z}] = true
+	}
+
+	return drops
+}
+
+func evalB(lines []string) int {
+	drops := getDropCoords(lines)
+	totalSurface := getTotalSurface(drops)
+	internalSurface := getInternalSurface(drops)
+
+	externalSurface := totalSurface - internalSurface
+
+	return externalSurface
+}
+
+func getInternalSurface(drops map[Coords]bool) int {
+	internalSpace := make(map[Coords]bool)
+	emptySpace := make(map[Coords]bool)
+	maxSearchSpace := getSearchSpace(drops)
+
+	for x := maxSearchSpace.xMin; x <= maxSearchSpace.xMax; x++ {
+		for y := maxSearchSpace.yMin; y <= maxSearchSpace.yMax; y++ {
+			for z := maxSearchSpace.zMin; z <= maxSearchSpace.zMax; z++ {
+				coordinate := Coords{x, y, z}
+				if drops[coordinate] || internalSpace[coordinate] || emptySpace[coordinate] {
+					continue
+				}
+				searchedSpace, isInternalSpace := checkIfInternalSpace(coordinate, drops, emptySpace, &maxSearchSpace)
+				if isInternalSpace {
+					for _, coords := range searchedSpace {
+						internalSpace[coords] = true
+					}
+				} else {
+					for _, coords := range searchedSpace {
+						emptySpace[coords] = true
+					}
+				}
+			}
+		}
+	}
+
+	return getTotalSurface(internalSpace)
+}
+
+func checkIfAppendToCoordsTOInvestigate(coordinate Coords, coordsToInvestigate []Coords, coordsToInvestigateAlreadyInSlice map[Coords]bool, drops map[Coords]bool, emptySpace map[Coords]bool) []Coords {
+	if !drops[coordinate] && !emptySpace[coordinate] && !coordsToInvestigateAlreadyInSlice[coordinate] {
+		coordsToInvestigate = append(coordsToInvestigate, coordinate)
+		coordsToInvestigateAlreadyInSlice[coordinate] = true
+	}
+
+	return coordsToInvestigate
+}
+
+func checkIfInternalSpace(startCoordinate Coords, drops map[Coords]bool, emptySpace map[Coords]bool, maxSearchSpace *Space) ([]Coords, bool) {
+	var coordsToInvestigate, searchedSpace []Coords
+	coordsToInvestigateAlreadyInSlice := make(map[Coords]bool)
+	coordsToInvestigate = append(coordsToInvestigate, startCoordinate)
+
+	for len(coordsToInvestigate) > 0 {
+		coordinate := coordsToInvestigate[0]
+		coordsToInvestigate = coordsToInvestigate[1:]
+		searchedSpace = append(searchedSpace, coordinate)
+		if drops[coordinate] || emptySpace[coordinate] {
+			continue
+		}
+
+		if coordinate.x == maxSearchSpace.xMin {
+			return searchedSpace, false
+		} else {
+			coordsToInvestigate = checkIfAppendToCoordsTOInvestigate(Coords{coordinate.x - 1, coordinate.y, coordinate.z}, coordsToInvestigate, coordsToInvestigateAlreadyInSlice, drops, emptySpace)
+		}
+		if coordinate.x == maxSearchSpace.xMax {
+			return searchedSpace, false
+		} else {
+			coordsToInvestigate = checkIfAppendToCoordsTOInvestigate(Coords{coordinate.x + 1, coordinate.y, coordinate.z}, coordsToInvestigate, coordsToInvestigateAlreadyInSlice, drops, emptySpace)
+		}
+		if coordinate.y == maxSearchSpace.yMin {
+			return searchedSpace, false
+		} else {
+			coordsToInvestigate = checkIfAppendToCoordsTOInvestigate(Coords{coordinate.x, coordinate.y - 1, coordinate.z}, coordsToInvestigate, coordsToInvestigateAlreadyInSlice, drops, emptySpace)
+		}
+		if coordinate.y == maxSearchSpace.yMax {
+			return searchedSpace, false
+		} else {
+			coordsToInvestigate = checkIfAppendToCoordsTOInvestigate(Coords{coordinate.x, coordinate.y + 1, coordinate.z}, coordsToInvestigate, coordsToInvestigateAlreadyInSlice, drops, emptySpace)
+		}
+		if coordinate.z == maxSearchSpace.zMin {
+			return searchedSpace, false
+		} else {
+			coordsToInvestigate = checkIfAppendToCoordsTOInvestigate(Coords{coordinate.x, coordinate.y, coordinate.z - 1}, coordsToInvestigate, coordsToInvestigateAlreadyInSlice, drops, emptySpace)
+		}
+		if coordinate.z == maxSearchSpace.zMax {
+			return searchedSpace, false
+		} else {
+			coordsToInvestigate = checkIfAppendToCoordsTOInvestigate(Coords{coordinate.x, coordinate.y, coordinate.z + 1}, coordsToInvestigate, coordsToInvestigateAlreadyInSlice, drops, emptySpace)
+		}
+	}
+
+	return searchedSpace, true
+}
+
+func getSearchSpace(drops map[Coords]bool) Space {
+	var xMin, xMax, yMin, yMax, zMin, zMax int
+	for coords := range drops {
+		xMin = util.MinInt(xMin, coords.x)
+		xMax = util.MaxInt(xMax, coords.x)
+		yMin = util.MinInt(yMin, coords.y)
+		yMax = util.MaxInt(yMax, coords.y)
+		zMin = util.MinInt(zMin, coords.z)
+		zMax = util.MaxInt(zMax, coords.z)
+	}
+	return Space{xMin, xMax, yMin, yMax, zMin, zMax}
 }
 
 func eval(filename string, debug bool) {
